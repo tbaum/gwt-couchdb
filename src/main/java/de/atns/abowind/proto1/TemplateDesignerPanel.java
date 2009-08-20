71541af1122ba12cdb028cca58398de050a12287
Thomas Baum <thomas.baum@atns.de>
2009-08-20 14:23:00 +0200

    - dnd controller

--- src/main/java/de/atns/abowind/proto1/TemplateDesignerPanel.java
+++ src/main/java/de/atns/abowind/proto1/TemplateDesignerPanel.java
@@ -2,6 +2,10 @@ package de.atns.abowind.proto1;
 
 import com.allen_sauer.gwt.dnd.client.AbstractDragController;
 import com.allen_sauer.gwt.dnd.client.DragController;
+import com.allen_sauer.gwt.dnd.client.drop.DropController;
+import com.allen_sauer.gwt.dnd.client.util.DOMUtil;
+import com.allen_sauer.gwt.dnd.client.util.Location;
+import com.allen_sauer.gwt.dnd.client.util.WidgetLocation;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.*;
@@ -44,8 +48,61 @@ public class TemplateDesignerPanel extends LayoutPanel {
     private final Tree currentTemplateTree = new Tree();
     private DragController dragController = new AbstractDragController(this) {
         public void dragMove() {
-            System.err.println("dragMove " + getElement());
+            // may have changed due to scrollIntoView() or developer driven changes
+            calcBoundaryOffset();
+
+            int desiredLeft = context.desiredDraggableX - boundaryOffsetX;
+            int desiredTop = context.desiredDraggableY - boundaryOffsetY;
+            if (getBehaviorConstrainedToBoundaryPanel()) {
+                desiredLeft = Math.max(0, Math.min(desiredLeft, dropTargetClientWidth
+                        - context.draggable.getOffsetWidth()));
+                desiredTop = Math.max(0, Math.min(desiredTop, dropTargetClientHeight
+                        - context.draggable.getOffsetHeight()));
+            }
+
+            DOMUtil.fastSetElementPosition(movablePanel.getElement(), desiredLeft, desiredTop);
+                        /*
+            DropController newDropController = getIntersectDropController(context.mouseX, context.mouseY);
+            if (context.dropController != newDropController) {
+                if (context.dropController != null) {
+                    context.dropController.onLeave(context);
+                }
+                context.dropController = newDropController;
+                if (context.dropController != null) {
+                    context.dropController.onEnter(context);
+                }
+            }
+                          */
+            if (context.dropController != null) {
+                context.dropController.onMove(context);
+            }
         }
+
+        private void calcBoundaryOffset() {
+            Location widgetLocation = new WidgetLocation(context.boundaryPanel, null);
+            boundaryOffsetX = widgetLocation.getLeft()
+                    + DOMUtil.getBorderLeft(context.boundaryPanel.getElement());
+            boundaryOffsetY = widgetLocation.getTop()
+                    + DOMUtil.getBorderTop(context.boundaryPanel.getElement());
+        }
+
+        private int boundaryOffsetX;
+
+        private int boundaryOffsetY;
+        private int dropTargetClientHeight;
+
+        private int dropTargetClientWidth;
+        private Widget movablePanel;
+
+        @Override
+        public void dragEnd() {
+
+            movablePanel.removeFromParent();
+            movablePanel = null;
+            super.dragEnd();
+        }
+
+
     };
 
     private DefaultComboBoxModel<TemplateSelection> templateSelection;
